5e6145cf60603900b58615a0859ec583542826e3
mszgajewski <mszgajewski@gmail.com>
2021-09-01 19:58:06 +0200

    hyperskill

---
+++ Topics/Declaring functions/Division/src/Main.kt
@@ -0,0 +1,10 @@
+
+fun main() {
+
+   // println("content".substringAfter("t"))
+            println("content".substring(1,1))
+            println("content".substringBefore("c"))
+           // println("content".substring(-1,-1))
+           // println("content".substring(0,1))
+            println("content".substring(0,0))
+}
--- Topics/Declaring functions/Vowel or not/src/Main.kt
+++ Topics/Declaring functions/Vowel or not/src/Main.kt
@@ -1,4 +1,10 @@
-// write your function here
+fun isVowel(letter: Char): Boolean {
+
+    return when (letter) {
+        'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' -> true
+        else -> false
+    }
+}
 
 fun main() {
     val letter = readLine()!!.first()
