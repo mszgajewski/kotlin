25f680f4da316558098fb9d5633e2b173f6d0b47
mszgajewski <mszgajewski@gmail.com>
2021-09-08 05:24:31 +0200

    hyperskill

--- Tic-Tac-Toe/task/src/tictactoe/Main.kt
+++ Tic-Tac-Toe/task/src/tictactoe/Main.kt
@@ -1,98 +1,155 @@
 package tictactoe
 
-import java.lang.Math.abs
+import java.util.*
+
+//_1x1 _1x2 _1x3 _2x1 _2x2 _2x3 _3x1 _3x2 _3x3
+
+fun printCurrentBoard(_1x1:Char, _1x2:Char, _1x3:Char,
+					  _2x1:Char, _2x2:Char, _2x3:Char,
+					  _3x1:Char, _3x2:Char, _3x3:Char) {
+
+	println("---------")
+	println("| $_1x1 $_1x2 $_1x3 |")
+	println("| $_2x1 $_2x2 $_2x3 |")
+	println("| $_3x1 $_3x2 $_3x3 |")
+	println("---------")
+}
 
 fun main() {
 
+	//Get starting stage of board
+	val  scn = Scanner(System.`in`)
 
-	println("Enter cells: ")
-	val ceels = readLine()!!
+	print("Enter cells: ")
 
-	val X = ceels.count{ c -> c == 'X' }
-	val O = ceels.count{ c -> c == 'O' }
+	val stringLineInput: String = scn.nextLine()
 
-	val row = ceels.toCharArray()
-	println("---------")
-	println("| ${row[0]} ${row[1]} ${row[2]} |")
-	println("| ${row[3]} ${row[4]} ${row[5]} |")
-	println("| ${row[6]} ${row[7]} ${row[8]} |")
-	println("---------")
+	var _1x1 : Char = stringLineInput[0]
+	var _1x2 : Char = stringLineInput[1]
+	var _1x3 : Char = stringLineInput[2]
 
-	println("Enter the coordinates: ")
+	var _2x1 : Char = stringLineInput[3]
+	var _2x2 : Char = stringLineInput[4]
+	var _2x3 : Char = stringLineInput[5]
 
-	val coordinates = readLine()!!
-	val (a, b) = coordinates.split(" ")
-	val xCor = a.toInt()
-	val yCor = b.toInt()
+	var _3x1 : Char = stringLineInput[6]
+	var _3x2 : Char = stringLineInput[7]
+	var _3x3 : Char = stringLineInput[8]
 
+	// print first board
+	printCurrentBoard(_1x1, _1x2, _1x3, _2x1, _2x2, _2x3, _3x1, _3x2, _3x3)
 
-	var rawNumber = coordinates.filter { it.isDigit() }
+	// init of var for condition validation
+	var wins = 0
+	var X = 0
+	var O = 0
+	var noErrors = false
+	// print board as long as no wincondition is met
+	while (noErrors == false) {
 
-	if (rawNumber.length < 2) {
-		println("You should enter numbers!")
-	} else if( xCor < 1 || xCor > 3 || yCor < 1 || yCor > 3) {
-		println("Coordinates should be from 1 to 3!")
-	}
-
-
-	if(xCor == 1 && yCor == 1 && row[0] != 'X' && row[0] != 'O') {
-		row[0] = 'X'
-	} else if (xCor == 1 && yCor == 2 && row[1] != 'X' && row[1] != 'O') {
-		row[1].equals("X")
-	} else if (xCor == 1 && yCor == 3 && row[2] != 'X' && row[2] != 'O') {
-		row[2].equals("X")
-	} else if (xCor == 2 && yCor == 1 && row[3] != 'X' && row[3] != 'O') {
-		row[3].equals("X")
-	} else if (xCor == 2 && yCor == 2 && row[4] != 'X' && row[4] != 'O') {
-		row[4].equals("X")
-	} else if (xCor == 2 && yCor == 3 && row[5] != 'X' && row[5] != 'O') {
-		row[5].equals("X")
-	} else if (xCor == 3 && yCor == 1 && row[6] != 'X' && row[6] != 'O') {
-		row[6].equals("X")
-	} else if (xCor == 3 && yCor == 2 && row[7] != 'X' && row[7] != 'O') {
-		row[7].equals("X")
-	} else if (xCor == 3 && yCor == 3 && row[8] != 'X' && row[8] != 'O') {
-		row[8].equals("X")
-	} else {
-		println("This cell is occupied! Choose another one!")
-	}
-
-	println("---------")
-	println("| ${row[0]} ${row[1]} ${row[2]} |")
-	println("| ${row[3]} ${row[4]} ${row[5]} |")
-	println("| ${row[6]} ${row[7]} ${row[8]} |")
-	println("---------")
+		// asks for next coordinates
+		print("Enter coordinates: ")
+		val newCoordinateToAdd: String = scn.nextLine()
+		val errorOccupied = "This cell is occupied! Choose another one!"
+		val errorNotOfCorrectType = "You should enter numbers"
+		val errorOutsideRange = "Coordinates should be from 1 to 3!"
 
 
+		// checks if user input is valid and sets noErrors to true else prints error messages
+		when (newCoordinateToAdd) {
+			"1 1" -> if (_1x1 == '_') {
+				_1x1 = 'X'
+				noErrors = true
+			} else println(errorOccupied)
+			"1 2" -> if (_1x2 == '_') {
+				_1x2 = 'X'
+				noErrors = true
+			} else println(errorOccupied)
+			"1 3" -> if (_1x3 == '_') {
+				_1x3 = 'X'
+				noErrors = true
+			} else println(errorOccupied)
+			"2 1" -> if (_2x1 == '_') {
+				_2x1 = 'X'
+				noErrors = true
+			} else println(errorOccupied)
+			"2 2" -> if (_2x2 == '_') {
+				_2x2 = 'X'
+				noErrors = true
+			} else println(errorOccupied)
+			"2 3" -> if (_2x3 == '_') {
+				_2x3 = 'X'
+				noErrors = true
+			} else println(errorOccupied)
+			"3 1" -> if (_3x1 == '_') {
+				_3x1 = 'X'
+				noErrors = true
+			} else println(errorOccupied)
+			"3 2" -> if (_3x2 == '_') {
+				_3x2 = 'X'
+				noErrors = true
+			} else println(errorOccupied)
+			"3 3" -> if (_3x3 == '_') {
+				_3x3 = 'X'
+				noErrors = true
+			} else println(errorOccupied)
+			else -> if (newCoordinateToAdd[0] > '3' || newCoordinateToAdd[2] > '3') {
+				println(errorOutsideRange)
+			} else println(errorNotOfCorrectType)
+		}
+		// prints board if validation above is true
+		if (noErrors){
+			printCurrentBoard(_1x1, _1x2, _1x3, _2x1, _2x2, _2x3, _3x1, _3x2, _3x3)
+		}
 /*
-	if (abs(X - O) > 1 || (row[0] == row[1] && row[1] == row[2]) && (row[3] == row[4] && row[4] == row[5])
-			|| (row[0] == row[1] && row[1] == row[2]) && (row[6] == row[7] && row[7] == row[9])
-			|| (row[6] == row[7] && row[7] == row[8]) && (row[3] == row[4] && row[4] == row[5])
-			|| (row[0] == row[3] && row[3] == row[6]) && (row[1] == row[4] && row[4] == row[7])
-			|| (row[0] == row[3] && row[3] == row[6]) && (row[2] == row[5] && row[5] == row[8])
-			|| (row[2] == row[4] && row[5] == row[8]) && (row[1] == row[4] && row[4] == row[7])
-			|| (row[0] == row[4] && row[4] == row[8]) && (row[2] == row[4] && row[4] == row[6]))
-	{ println("Impossible")
-	} else if((row[0].equals(row[1])) && ( row[1].equals(row[2])) ) {
-		println(row[0].toString() +" wins")
-	} else if((row[3].equals(row[4])) && ( row[5].equals(row[4])) ) {
-		println(row[3].toString() +" wins")
-	} else if((row[6].equals(row[7])) && ( row[7].equals(row[8])) ) {
-		println(row[6].toString() +" wins")
-	} else if((row[0].equals(row[3])) && ( row[3].equals(row[6])) ) {
-		println(row[0].toString() +" wins")
-	} else if((row[1].equals(row[7])) && ( row[7].equals(row[4])) ) {
-		println(row[1].toString() +" wins")
-	} else if((row[2].equals(row[5])) && ( row[5].equals(row[8])) ) {
-		println(row[2].toString() +" wins")
-	} else if((row[0].equals(row[4])) && ( row[4].equals(row[8])) ) {
-		println(row[0].toString() +" wins")
-	} else if((row[2].equals(row[4])) && ( row[4].equals(row[6])) ) {
-		println(row[2].toString() +" wins")
-	}  else if(X + O < 9) {
-		println("Game not finished")
-	}else {
-		println("Draw")
-	}
+		// Win conditions
+		val topHorWin = _1x1 == _1x2 && _1x1 == _1x3 && _1x1 != '_'
+		val midHorWin = _2x1 == _2x2 && _2x1 == _2x3 && _2x1 != '_'
+		val dowHorWin = _3x1 == _3x2 && _3x1 == _3x3 && _3x1 != '_'
+
+		val lefDowWin = _1x1 == _2x1 && _1x1 == _3x1 && _1x1 != '_'
+		val midDowWin = _1x2 == _2x2 && _1x2 == _3x2 && _1x2 != '_'
+		val rigDowWin = _1x3 == _2x3 && _1x3 == _3x3 && _1x3 != '_'
+
+		val sideWayWinLeftToRight = _1x1 ==_2x2 && _1x1 == _3x3 && _1x1 != '_'
+		val sideWayWinRightToLeft = _1x3 ==_2x2 && _1x3 == _3x1 && _1x3 != '_'
+
+		// check conditions
+		val winConditions = booleanArrayOf(topHorWin, midHorWin, dowHorWin, lefDowWin, midDowWin,
+			rigDowWin, sideWayWinLeftToRight, sideWayWinRightToLeft)
+
+		for (win in winConditions) {
+			if (win) {
+				wins++
+			}
+		}
+
+		for (i in stringLineInput) {
+			if (i == 'X') {
+				X++
+			} else if (i == 'O') {
+				O++
+			}
+		}
+
+		// prints out correct conditions
+		if (wins > 1 || X - O > 1 || O - X > 1) {
+			println("Impossible")
+		} else if (topHorWin || sideWayWinLeftToRight || lefDowWin) {
+			println("$_1x1 wins")
+		} else if (midHorWin) {
+			println("$_2x1 wins")
+		} else if (dowHorWin) {
+			println("$_3x1 wins")
+		} else if (sideWayWinRightToLeft || rigDowWin) {
+			println("$_1x3 wins")
+		} else if (midDowWin) {
+			println("$_1x2 wins")
+		} else if (!stringLineInput.contains('_', true)) {
+			println("Draw")
+		}
 	*/
+	}
+
+
 }
